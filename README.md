79fff9978f971c20f1269eb5e9eeb2e9bcb9f4b1
sazal <sazal@itconquest.com>
2022-03-18 01:48:15 +0600

    Add all files

---
+++ README.md
@@ -0,0 +1,43 @@
+# JP GROUP (jp-group)
+
+JP Group
+
+## Install Quasar on your device globally in terminal write this command
+
+```bash
+npm i -g @quasar/cli
+```
+
+## Install the dependencies
+
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+quasar dev
+```
+
+### Lint the files
+
+```bash
+npm run lint
+```
+
+### Format the files
+
+```bash
+npm run format
+```
+
+### Build the app for production
+
+```bash
+quasar build
+```
+
+### Customize the configuration
+
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,456 @@
+<template>
+  <q-page>
+    <div class="header">
+      <div class="q-px-xl q-py-xl">
+        <div class="row justify-between">
+          <div class="row">
+            <q-img
+              src="/images/LOGO.png"
+              class="headerLogo"
+              spinner-color="white"
+            />
+            <div class="text-white text-bold text-h3 q-ml-sm">JP GROUP</div>
+          </div>
+          <div class="row">
+            <q-btn label="Contace" no-caps flat color="white" size="lg" />
+            <q-btn class="q-ml-lg" size="lg" icon="menu" color="white" flat />
+          </div>
+        </div>
+      </div>
+      <div class="text-center" style="padding: 250px 0">
+        <div class="text-white text-h3">Creating spaces where future</div>
+        <div class="text-white text-h3">generations grow and prosper</div>
+        <div class="q-mt-lg text-white text-h5">
+          JP Group and it’s subsidiaries specialise in the Built Environment
+        </div>
+      </div>
+    </div>
+    <!-- Passion Part -->
+    <div class="passion_section">
+      <div class="text-center text-h4 text-bold">
+        Passion, Innovation, Performance.
+      </div>
+      <div class="q-px-xl passion_sub_title">
+        <div class="text-h5 text-bold">Creating spaces where future</div>
+        <div class="text-h5 text-bold">generations grow and prosper</div>
+      </div>
+      <div class="row q-px-xl">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="text-bold">
+            JP Group and it’s subsidiaries specialise in the Built Environment,
+            specifically new build homes. We are currently one of the
+            development partners for a Build to Rent fund, delivering 350 units
+            per year. We create sustainable, functional and future proofed
+            solutions that provide smartly designed spaces in which people can
+            live, work and enjoy on a daily basis.
+          </div>
+          <div class="q-py-lg text-bold">
+            Our bespoke approach will ensure projects achieve their maximum
+            potential from both a design and financial perspective, using the
+            latest techniques and technologies, and built to the highest
+            possible standards. Our in-house architectural, design and planning
+            team take the initial project vision from inception to completion,
+            assisting through the planning and design process, taking an initial
+            idea through to formal planning approval. Then our surveying and
+            project management team take over and oversee works through to
+            completion.
+          </div>
+          <div class="text-bold">
+            Operating throughout England and Wales from our Head Office in
+            Sheffield and our Manchester office, JP Group has extensive
+            experience within the industry, with a senior management team
+            focused on delivering projects to the highest possible standard.
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-8 text-right">
+          <q-img
+            src="/images/passionImage.png"
+            class="passion_image q-ma-sm"
+            spinner-color="white"
+          />
+        </div>
+      </div>
+    </div>
+    <!-- Chapter Part -->
+    <div class="chapter_scetion">
+      <div class="row q-pa-xl">
+        <div class="col-xs-12 col-sm-12 col-md-4 text-right">
+          <div class="chapter_title">
+            <div>
+              <div class="text-bold text-h5">Creating your</div>
+              <div class="text-bold text-h5">next chapter</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-8">
+          <div class="chapter_image_container">
+            <div class="chapter_right_image">
+              <div class="chapter_left">
+                <div class="">
+                  <div class="balton_logo"></div>
+                  <div class="text-bold q-pt-md">
+                    We select land with potential for development. At Belton
+                    Homes we source the land, finalise the permission and create
+                    amazing new build designs for upmost appeal, utilising
+                    future home building practices, like future ready heating
+                    systems and EV charging points in every development.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="memories_section row">
+        <div class="col-xs-12 col-sm-12 col-md-8">
+          <div class="memories_image_container">
+            <div class="memories_left_image"></div>
+            <div class="memories_left">
+              <div class="">
+                <div class="memories_logo"></div>
+                <div class="text-bold q-pt-md">
+                  Emerald construction specialising in new build homes, also
+                  residential conversions, and refurbishments. Working for
+                  clients across the UK.
+                </div>
+                <div class="text-bold q-pt-sm">
+                  At Emerald Construction we aspire to utilise the latest
+                  innovations in house building, providing future proof new
+                  homes for the next generations.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="memories_title">
+            <div>
+              <div class="text-bold text-h5">Building</div>
+              <div class="text-bold text-h5">memories for</div>
+              <div class="text-bold text-h5">future</div>
+              <div class="text-bold text-h5">generations</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- News Section -->
+    <div class="news_section">
+      <div class="text-h4 q-px-xl text-bold">News</div>
+      <div class="text-h4 text-bold text-center news_title">
+        Latest projects
+      </div>
+      <div class="project_container">
+        <div class="project q-ma-xl">
+          <div class="project_image1"></div>
+          <div class="project_desc">
+            <div>
+              <div class="text-h6">Project Barnsley</div>
+              <div class="text-h6">GDV £13m</div>
+              <div class="text-h6">70 Units</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="project_container">
+        <div class="project">
+          <div class="project_image2"></div>
+          <div class="project_desc">
+            <div>
+              <div class="text-h6">Project Keighley</div>
+              <div class="text-h6">GDV £16m</div>
+              <div class="text-h6">102 Units</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Get in touch -->
+    <div class="get_in_touch">
+      <div class="text-h4 text-bold text-center">Get in touch</div>
+      <div class="row get_in_touch_container">
+        <div class="col-sm-12 col-md-8">
+          <div class="row">
+            <div class="col-auto">
+              <q-avatar size="130px">
+                <img src="images/companyImage.png" />
+              </q-avatar>
+            </div>
+            <div class="col q-ml-md">
+              <div class="text-bold">JP Group</div>
+              <div class="text-bold">Unit 2, Phoenix Riverside</div>
+              <div class="text-bold">Sheffield Road</div>
+              <div class="text-bold">Rotherham</div>
+              <div class="text-bold">S60 1FL</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="q-mt-sm">
+            <q-btn icon="phone" flat color="green" />
+            <span class="text-bold">01142 684162</span>
+          </div>
+          <div class="q-mt-sm">
+            <q-btn icon="email" flat color="green" />
+            <span class="text-bold">admin@jpp-group.co.uk</span>
+          </div>
+          <div class="q-mt-md q-ml-md">
+            <q-btn label="Contact us" no-caps unelevated color="green" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Logo Section -->
+    <div class="logo_section">
+      <div class="logo1 q-mx-xs"></div>
+      <div class="logo2 q-mx-xs"></div>
+      <div class="logo3 q-mx-xs"></div>
+      <div class="logo4 q-mx-xs"></div>
+      <div class="logo5 q-mx-xs"></div>
+      <div class="logo6 q-mx-xs"></div>
+      <div class="logo7 q-mx-xs"></div>
+    </div>
+    <!-- Footer Section -->
+    <div class="footer row justify-between">
+      <div>
+        <div>
+          <a href="#">Home</a>
+        </div>
+        <div class="q-mt-sm">
+          <a href="#">Latest Projects</a>
+        </div>
+        <div class="q-mt-sm">
+          <a href="#">About Us</a>
+        </div>
+        <div class="q-mt-sm">
+          <a href="#">Subsidiaries</a>
+        </div>
+        <div class="q-mt-sm">
+          <a href="#">Contact</a>
+        </div>
+      </div>
+      <div>
+        <div>
+          <q-img
+            src="/images/mainLogo.png"
+            class="mainLogo q-ma-sm"
+            spinner-color="white"
+          />
+        </div>
+        <div class="text-right">
+          <q-btn round>
+            <q-avatar size="42px">
+              <img src="images/btn_icon.png" />
+            </q-avatar>
+          </q-btn>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
+
+<style>
+.header {
+  background: url(/images/headerImage.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 850px;
+}
+.passion_section {
+  padding: 70px 0;
+}
+.passion_sub_title {
+  padding-top: 50px;
+  padding-bottom: 30px;
+}
+.passion_image {
+  height: 330px;
+  max-width: 550px;
+  border-radius: 10px;
+}
+.chapter_scetion {
+  background-color: #e6eee5;
+  padding: 70px 0;
+}
+.chapter_image_container {
+  width: 90%;
+  margin-left: 40px;
+  display: flex;
+  justify-content: right;
+}
+.chapter_left {
+  width: 80%;
+  height: 400px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  margin-left: -130px;
+}
+.chapter_right_image {
+  background: url(/images/chapterImage.png) no-repeat center / contain;
+  height: 400px;
+  border-radius: 10px;
+  width: 70%;
+}
+.balton_logo {
+  background: url(/images/beltonHomeLogo.png) no-repeat center / contain;
+  height: 50px;
+  width: 50%;
+  margin: 0 auto;
+}
+.chapter_title {
+  display: flex;
+  justify-content: right;
+  height: 400px;
+  align-items: center;
+}
+.memories_section {
+  padding-top: 100px;
+}
+.memories_image_container {
+  width: 90%;
+  display: flex;
+  justify-content: center;
+}
+.memories_left_image {
+  background: url(/images/memoriesImage.png) no-repeat center / cover;
+  height: 400px;
+  border-radius: 10px;
+  width: 50%;
+}
+.memories_left {
+  width: 50%;
+  height: 400px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  margin-left: -303px;
+}
+.memories_logo {
+  background: url(/images/memoriesLogo.png) no-repeat center / contain;
+  height: 50px;
+  width: 50%;
+  margin: 0 auto;
+}
+.memories_title {
+  display: flex;
+  align-items: center;
+  height: 400px;
+}
+.news_section {
+  background-color: #eff3ef;
+  padding: 70px 0;
+}
+.news_title {
+  padding-top: 90px;
+  padding-top: 50px;
+}
+.project_container {
+  display: flex;
+  justify-content: center;
+}
+.project {
+  background-color: white;
+  border-radius: 10px;
+  height: 300px;
+  width: 50%;
+  display: flex;
+}
+.project_image1 {
+  width: 50%;
+  background: url(/images/projectImage1.png) no-repeat center / cover;
+  height: 100%;
+  border-radius: 10px;
+}
+.project_image2 {
+  width: 50%;
+  background: url(/images/projectImage2.png) no-repeat center / cover;
+  height: 100%;
+  border-radius: 10px;
+}
+.project_desc {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 50%;
+  padding-left: 40px;
+}
+.get_in_touch {
+  background-color: #e6eee5;
+  padding: 70px 0;
+}
+.get_in_touch_container {
+  width: 60%;
+  margin: 0 auto;
+  padding-top: 30px;
+}
+.logo_section {
+  padding: 70px 0;
+  display: flex;
+  justify-content: center;
+}
+.logo1 {
+  background: url(/images/beltonHomeLogo.png) no-repeat center / contain;
+  width: 170px;
+  height: 50px;
+}
+.logo2 {
+  background: url(/images/memoriesLogo.png) no-repeat center / contain;
+  width: 170px;
+  height: 50px;
+}
+.logo3 {
+  background: url(/images/logo3.png) no-repeat center / contain;
+  width: 70px;
+  height: 50px;
+}
+.logo4 {
+  background: url(/images/logo4.png) no-repeat center / contain;
+  width: 100px;
+  height: 50px;
+}
+.logo5 {
+  background: url(/images/logo5.png) no-repeat center / contain;
+  width: 120px;
+  height: 50px;
+}
+.logo6 {
+  background: url(/images/logo6.png) no-repeat center / contain;
+  width: 50px;
+  height: 50px;
+}
+.logo7 {
+  background: url(/images/logo7.png) no-repeat center / contain;
+  width: 60px;
+  height: 50px;
+}
+.footer {
+  background-color: black;
+  padding: 70px 70px;
+}
+a {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+}
+.mainLogo {
+  width: 320px;
+}
+.headerLogo {
+  width: 85px;
+}
+</style>
